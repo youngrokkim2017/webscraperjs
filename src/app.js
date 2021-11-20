20c2fc8916849fd180ae20521e08ae16a3e4f670
Patrick Kim <patrick.yrkim@gmail.com>
2021-11-19 21:13:35 -0800

    Add webscrape data to client side render

--- src/app.js
+++ src/app.js
@@ -3,4 +3,16 @@ const feedDisplay = document.querySelector('#feed')
 
 fetch('http://localhost:8000/results')
 .then(response => response.json())
-.then(data => console.log(data))
+// .then(data => console.log(data))
+.then(data => {
+    data.forEach(article => {
+        // const title = `<h3>` + article.title + `</h3>`
+        // feedDisplay.insertAdjacentHTML("beforeend", title);
+
+        const articleItem = `<div><h3>` + article.title + `</h3><p>` + article.url + `</p></div>`
+        feedDisplay.insertAdjacentHTML("beforeend", articleItem);
+        // insertAjacentHTML passes the specific text in as html and inserts it into the DOM tree
+        // beforeend inserts after the last child
+    })
+})
+.catch(err => console.log(err))
